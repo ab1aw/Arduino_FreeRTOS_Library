d5f3832ae69046ecd72e2c5fcd5d07981df96389
Michael Polia <mpolia@medacuity.com>
2024-03-20 13:35:32 -0400

    Modifications to the example for demonstrating assertion handling.

--- examples/Assert/Assert.ino
+++ examples/Assert/Assert.ino
@@ -3,12 +3,21 @@
  * https://www.freertos.org/a00110.html#configASSERT
  */
 
-#include <Arduino_FreeRTOS.h>
+#include <Arduino.h>
+#include <Adafruit_TinyUSB.h> // for Serial
 
 const boolean valueToAssert = true;
 
 // The setup function runs once when you press reset or power the board
 void setup() {
+
+    Serial.begin(115200);
+
+    // Wait for a serial port connection to be established before continuing.
+    // Don't want to miss any debug messages.
+    while ( !Serial ) delay(10);   // for nrf52840 with native usb
+
+    Serial.println("STARTING THE APPLICATION.");
 
   // Assert value is true, execution doesn't stop.
   configASSERT(valueToAssert == true);
